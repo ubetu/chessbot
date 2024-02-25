33bc6b913364eb5ac5c299546c5618ff56043203
ubetu <pav.s2012@mail.ru>
2024-02-26 00:05:39 +0300

    fsm start

--- chess_api.py
+++ chess_api.py
@@ -1,22 +1,22 @@
 import chess
 import chess.svg
-import os
 import wand.image
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
-class Game:
+from random import choice
+class GameManager:
     def __init__(self):
         self.board = chess.Board()
+        self.color = choice((chess.WHITE, chess.BLACK))
+        self.color_text = 'Белые' if self.color == chess.WHITE else 'Черные'
+
     def is_legal_move(self, move: str) -> bool:
         try:
             return self.board.is_legal(self.board.parse_san(move))
         except chess.InvalidMoveError:
             return False
-    def create_image(self) -> None:
+    def create_image(self) -> bytes:
         with wand.image.Image(blob=chess.svg.board(board=self.board).encode('utf-8'), format="svg") as image:
             png_image = image.make_blob("png")
-            with open("board.png", 'wb') as board_image:
-                board_image.write(png_image)
+            #with open("board.png", 'wb') as board_image:
+                #board_image.write(png_image)
+        return png_image
 
-game = Game()
-game.create_image()
--- handlers.py
+++ handlers.py
@@ -1,10 +1,15 @@
 import kb
 import text
+import chess_api
 
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
+class GameFSM(StatesGroup):
+    process = State()
 
 router_main = Router()
 
@@ -13,5 +18,9 @@ async def start(message: Message):
     await message.answer(text.greeting, reply_markup=kb.new_game_kb)
 
 @router_main.message(F.text == text.new_game_button)
-async def new_game(message: Message):
+async def new_game(message: Message, state: FSMContext):
     await message.answer(text.new_game_started)
+    await state.set_state(GameFSM.process)
+    game = chess_api.GameManager()
+    await state.update_data(game=game)
+    await message.answer(text.greeting + game.color_text)
